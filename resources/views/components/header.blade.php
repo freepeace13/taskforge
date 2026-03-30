ad18307ddaec4102dbbe5b8fe81f9494dcaf5d94
Kin Basco <freepeace13@gmail.com>
2026-03-30 11:23:11 +0800

    Add login link

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -24,7 +24,7 @@
 
       <div class="flex items-center gap-3">
         <a
-          href="#"
+          href="{{ route('login') }}"
           class="hidden rounded-2xl px-4 py-2 text-sm font-semibold text-slate-700 transition hover:bg-slate-100 sm:inline-flex"
         >
           Sign in
